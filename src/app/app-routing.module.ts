5642195c466de91f35a0db3418e1c49be1e803c9
Ferdyan A. Y. Irawan <flamesmoss57@gmail.com>
2021-04-06 15:48:30 +0700

    Hello world

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'register',
+    component: RegisterComponent
+  },
+  {
+    path:'admin',
+    loadChildren:=>import('./admin/admin.module').then(mod=>mod.AdminModule)
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+function impor(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
